92be16459f84edcb22ca3ee235c81bf4d81f7133
mohammedk91 <mohammed.k.meshkawi@gmail.com>
2018-03-06 20:47:14 +0100

    Update functionality & add specs

--- lib/binary.rb
+++ lib/binary.rb
@@ -3,7 +3,7 @@ require "binary/version"
 module Binary
 	def self.binary(input=nil)
 		if input.class == Array
-			input.map { |num| num.to_s(2) }
+			input.map { |num| num.class == Integer ? num.to_s(2) : nil }
     elsif input.class == Integer
       input.to_s(2)
     end
--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Binary do
   it "takes an integer and returns a binary representation" do
     expect(Binary.binary(7)).to eq("111")
   end
+
+  it "non-integer input should return nil" do
+    expect(Binary.binary('hey')).to eq(nil)
+  end
+
+  it "takes an array of numbers and returns an array of binary values" do
+    expect(Binary.binary([1,2,3])).to eq(["1", "10", "11"])
+  end
+
+  it "each non-integer element in the array input should return nil" do
+    expect(Binary.binary(['one','two',3])).to eq([nil, nil, "11"])
+  end
 end
