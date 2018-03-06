277940a07821c8ea096cf7325b907753b7be5a6a
mohammedk91 <mohammed.k.meshkawi@gmail.com>
2018-03-06 19:56:55 +0100

    accept array of integers as in input

--- lib/binary.rb
+++ lib/binary.rb
@@ -1,9 +1,11 @@
 require "binary/version"
 
 module Binary
-	def self.binary(number=nil)
-    if number.class == Integer
-      number.to_s(2)
+	def self.binary(input=nil)
+		if input.class == Array
+			input.map { |num| num.to_s(2) }
+    elsif input.class == Integer
+      input.to_s(2)
     end
   end
 end
