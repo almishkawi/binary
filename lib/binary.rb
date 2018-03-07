ee3de2dc60e05658b27c993a39940972dcae60fc
mohammedk91 <mohammed.k.meshkawi@gmail.com>
2018-03-07 11:46:33 +0100

    cleanup

--- lib/binary.rb
+++ lib/binary.rb
@@ -1,11 +1,11 @@
 require "binary/version"
 
 module Binary
-	def self.binary(input=nil)
-		if input.class == Array
-			input.map { |num| num.class == Integer ? num.to_s(2) : nil }
-    elsif input.class == Integer
-      input.to_s(2)
+  def self.binary(input=nil)
+    if input.class == Array
+      input.map { |num| num.class == Integer ? num.to_s(2) : nil }
+    else
+      input.class == Integer ? input.to_s(2) : nil
     end
   end
 end
