3b2ad81d88b3039efb305e8a7a3cf39fa0ae7337
mohammedk91 <mohammed.k.meshkawi@gmail.com>
2018-03-23 18:12:00 +0100

    Update binary method

--- lib/binary.rb
+++ lib/binary.rb
@@ -1,11 +1,15 @@
 require "binary/version"
+require "prime"
 
 module Binary
   def self.binary(input=nil)
     if input.class == Array
       input.map { |num| num.class == Integer ? num.to_s(2) : nil }
+    elsif input.class == Integer
+      input = input.abs if input < 0
+      input.to_s(2)
     else
-      input.class == Integer ? input.to_s(2) : nil
+      nil
     end
   end
 end
