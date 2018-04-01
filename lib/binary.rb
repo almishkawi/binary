ed41eba2290f7b6579575d2b24934e4ad12da8f9
mohammedk91 <mohammed.k.meshkawi@gmail.com>
2018-04-01 13:29:10 +0200

    New method "number" and some small updates

--- lib/binary.rb
+++ lib/binary.rb
@@ -4,7 +4,7 @@ require "prime"
 module Binary
   def self.binary(input=nil)
     if input.class == Array
-      input.map { |num| num.class == Integer ? num.to_s(2) : nil }
+      input.map { |num| num.class == Integer ? num.abs.to_s(2) : nil }
     elsif input.class == Integer
       input = input.abs if input < 0
       input.to_s(2)
@@ -13,26 +13,47 @@ module Binary
     end
   end
 
-  def self.bits num
-    binary = binary(num) if num
+  def self.number(input=nil)
+    if input.class == Array
+      input.map { |num| num.class == String ? num.to_i(2) : nil }
+    elsif input.class == String
+      input.to_i(2)
+    else
+      nil
+    end
+  end
+
+  def self.bits num=nil
+    return nil unless num
+    binary = binary(num)
     binary.length
   end
 
-  def self.zeros num
-    binary = binary(num) if num
+  def self.zeros num=nil
+    return nil unless num
+    binary = binary(num)
     binary.count("0")
   end
 
-  def self.ones num
-    binary = binary(num) if num
+  def self.ones num=nil
+    return nil unless num
+    binary = binary(num)
     binary.count("1")
   end
 
-  def self.prime num
+  def self.prime num=nil
+    return nil unless num
     binaries = []
     Prime.each(num) do |prime|
       binaries << binary(prime)
     end
     binaries
+  end
+
+  def self.method_missing(method, *args, &block)
+    puts "There is no method called #{method} in Binary."
+    p 'methods available are:'
+    ['binary', 'number', 'bit', 'zeros', 'ones', 'prime'].each { |m| p "#{m}" }
+    nil
   end
 end
--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -3,20 +3,28 @@ RSpec.describe Binary do
     expect(Binary::VERSION).not_to be nil
   end
 
-  it "takes an integer and returns a binary representation" do
+  it "takes an integer and returns its binary representation" do
     expect(Binary.binary(7)).to eq("111")
   end
 
-  it "non-integer input should return nil" do
+  it "takes non-integer input and returns nil" do
     expect(Binary.binary('hey')).to eq(nil)
   end
 
-  it "takes an array of numbers and returns an array of binary values" do
+  it "takes an array of numbers and returns an array of their binary values" do
     expect(Binary.binary([1,2,3])).to eq(["1", "10", "11"])
   end
 
-  it "each non-integer element in the array input should return nil" do
+  it "takes an array of numbers and returns nil for each non-integer element in the array" do
     expect(Binary.binary(['one','two',3])).to eq([nil, nil, "11"])
+  end
+
+  it "takes a binary string and returns its integer representation" do
+    expect(Binary.number("111")).to eq(7)
+  end
+
+  it "takes an array of binary strings and returns an array of their integer values" do
+    expect(Binary.number(["1", "10", "11"])).to eq([1,2,3])
   end
 
   it "takes an integer and returns number of bits in its binary representation" do
